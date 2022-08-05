c081591591f906ace40f557bd7d480746e82de7d
Filipe Cavalcanti <filipe-cavalcanti@outlook.com>
2022-08-05 12:19:01 -0300

    Criar CRUD Loja de Games

---
+++ src/main/java/br/org/generation/lojaDeGames/model/Produto.java
@@ -0,0 +1,70 @@
+package br.org.generation.lojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 255, message = "O atributo nome deve conter no mínimo de 05 e no máximo 255 caracteres")
+	private String nome;
+
+	@NotNull
+	@Min(value = 1)
+	@Max(value = 500)
+	private Integer estoque;
+
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
